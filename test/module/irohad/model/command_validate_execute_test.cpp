ae53895d99e74fca8f5b4835577f9c585898b27c
grimadas <bulat.nasrulin@gmail.com>
2017-09-22 14:20:27 +0800

    Add insert Role Permissions wsv command

--- test/module/irohad/model/command_validate_execute_test.cpp
+++ test/module/irohad/model/command_validate_execute_test.cpp
@@ -48,6 +48,8 @@ class CommandValidateExecuteTest : public ::testing::Test {
  public:
 
   void SetUp() override {
+    spdlog::set_level(spdlog::level::off);
+
     factory = CommandExecutorFactory::create().value();
 
     wsv_query = std::make_shared<StrictMock<MockWsvQuery>>();
@@ -743,6 +745,7 @@ class CreateRoleTest: public CommandValidateExecuteTest {
 
 TEST_F(CreateRoleTest, VadlidCase){
   EXPECT_CALL(*wsv_command, insertRole(_)).WillOnce(Return(true));
+  EXPECT_CALL(*wsv_command, insertRolePermissions(_, _)).WillOnce(Return(true));
   ASSERT_TRUE(validateAndExecute());
 }
 
